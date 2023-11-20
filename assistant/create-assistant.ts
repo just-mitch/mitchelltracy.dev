c05a361a6633c2cf95ae9741058c7e38218850b2
Mitchell Tracy <mitch@twnsqr.com>
2023-11-20 15:19:49 -0500

    initial commit

---
+++ assistant/create-assistant.ts
@@ -0,0 +1,62 @@
+import * as fs from "fs";
+import { readFile, writeFile } from "fs/promises";
+import OpenAI from "openai";
+
+// Heads up. Run this with `bun create-assistant` in the project root.
+
+const openai = new OpenAI();
+const basePath = "assistant";
+const cachePath = `${basePath}/assistant.json`;
+const knowledgeBase = [`${basePath}/project.txt`, `${basePath}/resume.txt`];
+const description =
+  "You are the partner of Mitchell Tracy, a software engineer. People are asking you questions about him, his experience, and his personality. You answer the questions honestly, and in an manner that reflects his excitement for learning and growth in himself and his team.";
+
+function loadKnowledgeBaseFiles() {
+  return Promise.all(
+    knowledgeBase.map(async (filename) =>
+      openai.files.create({
+        file: fs.createReadStream(filename),
+        purpose: "assistants",
+      })
+    )
+  );
+}
+
+async function createAssistant() {
+  const files = await loadKnowledgeBaseFiles();
+
+  return openai.beta.assistants.create({
+    name: "Mitch Assistant",
+    description,
+    model: "gpt-4-1106-preview",
+    tools: [{ type: "retrieval" }],
+    file_ids: files.map((file) => file.id),
+  });
+}
+
+async function loadOrCreateAssistantId() {
+  const cache = await readFile(cachePath, { encoding: "utf-8" }).catch(
+    () => null
+  );
+
+  if (cache) {
+    const { assistantId } = JSON.parse(cache);
+    if (assistantId) {
+      console.log("Loaded assistant id from cache", assistantId);
+      return assistantId;
+    }
+  }
+
+  const assistant = await createAssistant();
+  const assistantId = assistant.id;
+  console.log("Created assistant", assistantId);
+  // flush the assistant id to json
+  await writeFile(cachePath, JSON.stringify({ assistantId }));
+  return assistantId;
+}
+
+async function main() {
+  await loadOrCreateAssistantId();
+}
+
+main();
---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Mitchell Tracy',
+    default: 'Mitchell Tracy - Software engineer',
+  },
+  description:
+    'I’m Mitch, a software engineer based in Maryland. I am searching for a full-time position at a company that is developing advanced technology in pursuit of brighter future.',
+  alternates: {},
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
